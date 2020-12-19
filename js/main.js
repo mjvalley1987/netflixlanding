74a1663d9b2a7fad2f11a3f9ac753beff84cb5e5
mjvalley1987 <74330239+mjvalley1987@users.noreply.github.com>
2020-12-19 17:08:20 -0500

    Add files via upload
    initial files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+//select tab content item
+function selectItem(e) {
+    //remove all show and border classes
+    removeBorder();
+    removeShow();
+    //add border to current tab
+    this.classList.add('tab-border')
+    //Grab content item from DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`)
+    //add show class
+    tabContentItem.classList.add('show')
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'))
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'))
+}
+
+//listen for tab click
+tabItems.forEach(item => item.addEventListener('click',selectItem))
